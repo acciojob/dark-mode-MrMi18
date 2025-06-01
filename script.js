fc33d2ab99f131d12e09e81c5cbf8405817acd1d
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-06-01 10:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 //your JS code here. If required.
-localStorage.setItem("preference", "DarkMode");
+localStorage.setItem("preference", { name: 'John Doe', email: 'john@example.com', phone: '555-555-5555' });
