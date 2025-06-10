9554469c2de3925a769826bfab3021b21d84c2c7
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-06-10 09:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 //your JS code here. If required.
-localStorage.setItem("preference", { name: 'John Doe', email: 'john@example.com', phone: '555-555-5555' });
+localStorage.setItem("preference", { name: 'John Doe', email: 'john@example.com', phone: '555-555-5555',theme:'dark mode' });
